f9d954acfe99e92e87bc1750724d0bcbb8cc8826
hy22cjin <114383440+Kasmesq@users.noreply.github.com>
2025-11-10 08:44:54 -0500

    Delete py.py

--- py.py
+++
@@ -1,4 +0,0 @@
-import torch, importlib
-print("torch:", torch.__version__, "cuda build:", torch.version.cuda)
-print("datastates import OK:", importlib.import_module("datastates"))
-print("ckpt ext import OK:", importlib.import_module("datastates.ckpt.src"))
